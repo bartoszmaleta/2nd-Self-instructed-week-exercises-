0d81b98c373a573502a9410fca13a02898fb843e
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-12 17:05:23 +0200

    Changed variables names

--- ideabank.py
+++ ideabank.py
@@ -1,21 +1,21 @@
-# ideabank = open('test.txt', 'a')
+# ideabank = open('bank_of_ideas.txt', 'a')
 # idea = input('What is your new idea: ')
 
 # ideabank.write(idea)
 # ideabank.close()
 
-# ideabank = open('test.txt', 'r')
+# ideabank = open('bank_of_ideas.txt', 'r')
 # for line in ideabank:
 #     print(line)
 
 # print(ideabank.read())
 
-with open("test.txt", "a") as ideabank:
+with open("bank_of_ideas.txt", "a") as ideabank:
     idea = input("what is your new idea? ")
     ideabank.write(idea + "\n") 
    
 
-with open("test.txt", "r") as ideabank:
+with open("bank_of_ideast.txt", "r") as ideabank:
     for i, line in enumerate(ideabank):
         
         print(f'{format(i+1)} {line.strip()}')
